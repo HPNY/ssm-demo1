fe974d21e2a36d0b58c26869ae1e1bae1ca0d2cb
Whisper0721 <30337883+Whisper0721@users.noreply.github.com>
2018-07-24 14:51:02 +0800

    Add files via upload

---
+++ test/src/main/java/com/test/entity/Appointment.java
@@ -0,0 +1,69 @@
+package com.test.entity;
+
+import java.util.Date;
+
+/**
+ * 预约图书实体
+ */
+public class Appointment {
+
+	private long bookId;//图书实体
+	
+	private long studentId;//学号
+	
+	private Date appointTime;//预约时间
+	
+	//多对一的复合属性
+	private Book book;//图书实体
+
+	public long getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+
+	public long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(long studentId) {
+		this.studentId = studentId;
+	}
+
+	public Date getAppointTime() {
+		return appointTime;
+	}
+
+	public void setAppointTime(Date appointTime) {
+		this.appointTime = appointTime;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public Appointment() {
+		super();
+	}
+
+	public Appointment(long bookId, long studentId, Date appointTime, Book book) {
+		super();
+		this.bookId = bookId;
+		this.studentId = studentId;
+		this.appointTime = appointTime;
+		this.book = book;
+	}
+
+	@Override
+	public String toString() {
+		return "Appointment [bookId=" + bookId + ", studentId=" + studentId + ", appointTime=" + appointTime + ", book="
+				+ book + "]";
+	}
+	
+}
---
+++ test/src/main/java/com/test/service/impl/BookServiceImplTest.java
@@ -0,0 +1,22 @@
+package com.test.service.impl;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.test.BaseTest;
+import com.test.dto.AppointExecution;
+import com.test.service.BookService;
+
+public class BookServiceImplTest extends BaseTest {
+
+	@Autowired
+    private BookService bookService;
+
+    @Test
+    public void testAppoint() throws Exception {
+        long bookId = 1001;
+        long studentId = 12345678910L;
+        AppointExecution execution = bookService.appoint(bookId, studentId);
+        System.out.println(execution);
+    }
+}
